34ad9c7e0089dbfe675a3d84b62c7b55c8d159d2
Solis3D <127130916+Solis3D@users.noreply.github.com>
2026-02-13 19:07:44 +0100

    layout adjustmens

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,4 +1,4 @@
-import { Container, Navbar, Form, Button, Image } from "react-bootstrap";
+import { Container, Navbar, Form, Button, Image, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logoMeteo from "../assets/images/logoMeteo.png";
 
@@ -11,24 +11,28 @@ const MyNavbar = function (props) {
   return (
     <>
       <Navbar bg="dark-subtle" data-bs-theme="dark">
-        <Container fluid className="px-3 py-1">
-          <Link to="/">
-            <Image src={logoMeteo} style={{ width: "150px" }} className="me-4"></Image>
-          </Link>
-          <Form onSubmit={handleSubmit} className=" d-flex gap-2">
-            <Form.Control
-              id="location-search"
-              type="text"
-              placeholder="City..."
-              className=" mr-sm-2"
-              value={props.inputCity}
-              onChange={(e) => props.setInputCity(e.target.value)}
-            />
-            <Button type="submit" variant="dark">
-              Search
-            </Button>
-          </Form>
-        </Container>
+        <Row className=" justify-content-between align-items-center w-100 g-2 g-md-2 py-2 py-md-0">
+          <Col xs="4" md="4" lg="2">
+            <Link to="/">
+              <Image fluid src={logoMeteo} className="ms-1 "></Image>
+            </Link>
+          </Col>
+          <Col xs="7" md="6" lg="5">
+            <Form onSubmit={handleSubmit} className=" d-flex gap-2">
+              <Form.Control
+                id="location-search"
+                type="text"
+                placeholder="City..."
+                className=" mr-sm-2"
+                value={props.inputCity}
+                onChange={(e) => props.setInputCity(e.target.value)}
+              />
+              <Button type="submit" variant="dark" className="py-md-3 px-md-5">
+                Search
+              </Button>
+            </Form>
+          </Col>
+        </Row>
       </Navbar>
     </>
   );
